4022ed8960eb9b6a8c4c3cd68a24ac8c3f8ded0d
Kevin Stewart <kevin.g.stewart@gmail.com>
2023-06-19 16:23:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,23 +6,23 @@
 ## Purpose: Wrapper for Dehydrated Acme client to simplify usage on F5 BIG-IP
 ##
 ## Usage:
-## - Execute: curl https://raw.githubusercontent.com/kevingstewart/simple-dehydrated-acme/main/install.sh | bash
+## - Execute: curl -s https://raw.githubusercontent.com/kevingstewart/simple-dehydrated-acme/main/install.sh | bash
 ##
 
 ## Create working directory
-mkdir -p /shared/acme/wellknown
+mkdir -p /shared/acme/wellknown > /dev/null 2>&1
 
 ## Download and place files
-curl https://raw.githubusercontent.com/dehydrated-io/dehydrated/master/dehydrated -o /shared/acme/dehydrated && chmod +x /shared/acme/dehydrated
-curl https://raw.githubusercontent.com/kevingstewart/simple-dehydrated-acme/main/config -o /shared/acme/config
-curl https://raw.githubusercontent.com/kevingstewart/simple-dehydrated-acme/main/domains.txt -o /shared/acme/domains.txt
-curl https://raw.githubusercontent.com/kevingstewart/simple-dehydrated-acme/main/hook_script_f5.sh -o /shared/acme/hook_script_f5.sh && chmod +x /shared/acme/hook_script_f5.sh
+curl -s https://raw.githubusercontent.com/dehydrated-io/dehydrated/master/dehydrated -o /shared/acme/dehydrated && chmod +x /shared/acme/dehydrated
+curl -s https://raw.githubusercontent.com/kevingstewart/simple-dehydrated-acme/main/config -o /shared/acme/config
+curl -s https://raw.githubusercontent.com/kevingstewart/simple-dehydrated-acme/main/domains.txt -o /shared/acme/domains.txt
+curl -s https://raw.githubusercontent.com/kevingstewart/simple-dehydrated-acme/main/hook_script_f5.sh -o /shared/acme/hook_script_f5.sh && chmod +x /shared/acme/hook_script_f5.sh
 
 ## Create BIG-IP data group
-tmsh create ltm data-group internal acme_handler_dg type string
+tmsh create ltm data-group internal acme_handler_dg type string > /dev/null 2>&1
 
 ## Create BIG-IP iRule
-tmsh create ltm rule acme_handler_rule when HTTP_REQUEST priority 2 {if { [string tolower [HTTP::uri]] starts_with \"/.well-known/acme-challenge/\" } {set response_content [class lookup [substr [HTTP::uri] 28] acme_handler_dg]\;if { \$response_content ne \"\" } { HTTP::respond 200 -version auto content \$response_content noserver Content-Type {text/plain} Content-Length [string length \$response_content] Cache-Control no-store } else { HTTP::respond 503 -version auto content \"\<html\>\<body\>\<h1\>503 - Error\<\/h1\>\<p\>Content not found.\<\/p\>\<\/body\>\<\/html\>\" noserver Content-Type {text/html} Cache-Control no-store }\;unset response_content\;event disable all\;return}}
+tmsh create ltm rule acme_handler_rule when HTTP_REQUEST priority 2 {if { [string tolower [HTTP::uri]] starts_with \"/.well-known/acme-challenge/\" } {set response_content [class lookup [substr [HTTP::uri] 28] acme_handler_dg]\;if { \$response_content ne \"\" } { HTTP::respond 200 -version auto content \$response_content noserver Content-Type {text/plain} Content-Length [string length \$response_content] Cache-Control no-store } else { HTTP::respond 503 -version auto content \"\<html\>\<body\>\<h1\>503 - Error\<\/h1\>\<p\>Content not found.\<\/p\>\<\/body\>\<\/html\>\" noserver Content-Type {text/html} Cache-Control no-store }\;unset response_content\;event disable all\;return}}  > /dev/null 2>&1
 
 ## Create scheduling
 
