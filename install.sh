49e545cee43b069499a1360a477738d5fa7e5356
Kevin Stewart <kevin.g.stewart@gmail.com>
2023-10-06 13:57:37 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ## Simple Dehydrated Acme (F5 BIG-IP) - Install Utility
 ## Maintainer: kevin-at-f5-dot-com
-## Version: 20230901-1
+## Version: 20231006-1
 ## Description: Wrapper for Dehydrated Acme client to simplify usage on F5 BIG-IP
 ##
 ## Usage:
@@ -29,6 +29,10 @@ tmsh create ltm data-group internal dg_acme_config type string > /dev/null 2>&1
 
 ## Create BIG-IP iRule (acme_handler_rule)
 tmsh create ltm rule acme_handler_rule when RULE_INIT { set static::DEBUGACME 0 }\;when HTTP_REQUEST priority 2 {if { [string tolower [HTTP::uri]] starts_with \"/.well-known/acme-challenge/\" } {set response_content [class lookup [substr [HTTP::uri] 28] dg_acme_challenge]\;if { \$response_content ne \"\" } { if { \$static::DEBUGACME } { log local0. \"[IP::client_addr]:[TCP::client_port]-[IP::local_addr]:[TCP::local_port] Good ACME response: \$response_content\" }\;HTTP::respond 200 -version auto content \$response_content noserver Content-Type {text/plain} Content-Length [string length \$response_content] Cache-Control no-store } else { if { \$static::DEBUGACME } { log local0. \"[IP::client_addr]:[TCP::client_port]-[IP::local_addr]:[TCP::local_port] Bad ACME request\" }\;HTTP::respond 503 -version auto content \"\<html\>\<body\>\<h1\>503 - Error\<\/h1\>\<p\>Content not found.\<\/p\>\<\/body\>\<\/html\>\" noserver Content-Type {text/html} Cache-Control no-store }\;unset response_content\;event disable all\;return}} > /dev/null 2>&1
+
+
+## Copy ca-bundle.crt to working directory
+cp $(tmsh list sys file ssl-cert ca-bundle.crt -hidden |grep cache-path | sed -E 's/^\s+cache-path\s//') /shared/acme/ca-bundle.crt
 
 
 ## Create the log file
